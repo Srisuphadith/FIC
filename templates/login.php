c133e5bef1dcf9a2e82afda3aa1a539856898d0e
FiFaKuBz <fifa.yanakorn@gmail.com>
2023-12-01 21:22:56 +0700

    mainPage

--- templates/login.php
+++ templates/login.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
+    <title>FIC</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <h1>Hello world</h1>
+
+<body class="login">
+    <div class="login">
+    <h1 class="header">Login</h1>
+</div>
 </body>
+
 </html>
