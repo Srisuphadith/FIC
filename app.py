241f9dfc5b18347f3f3826362b9b79c67bd9c867
Nine <ninerattana2005@gmail.com>
2023-12-01 16:40:45 +0700

    setup api and dashboard
    create api to communicate with esp8266  and add dashboard

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask
-
+from flask import render_template,request
+import json
 app = Flask(__name__)
 
-@app.route("/")
+#web dashboard
+@app.route("/" ,methods=["GET", "POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    if request.method == "POST":
+        #username = request.form['student_id']
+        print("Hello")
+    return render_template("index.html") # open file index.html 
 
-@app.route("/me")
-def me_api():
-    return {
-        "username": "hello",
-        "theme": "dark",
-        "image": "img",
-    }
+#REST API
+
+#API for control esp8266
+@app.route('/data_request', methods=['GET'])
+def data_request():
+    if request.method == "GET":
+         return {"sw_status" : 0,},200
+    else:
+        return {"status" : "404 err"},404
+
+#API for esp8266 send data to server
+@app.route('/data_upload', methods=['POST'])
+def data_upload():
+    if request.method == "POST":
+        data = json.loads(request.data)
+        print(data)
+        return data,200
+    else:
+        return {"status" : "404 err"},404
