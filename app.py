6b09b79163ea1c08f9cb6cf3886199cd77047adc
Nine <ninerattana2005@gmail.com>
2023-12-02 13:40:42 +0700

    esp8266 request and save to database
    use xampp to simulate mysql server
    esp8266 - methods post complete-methods get not yet
    vercel เป็นไรไม่รู้เรียก esp8266 เรียยก API ไม่ได้ เลยใช้เเบบ local ไปก่อน

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template,request
 import json
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -29,7 +30,29 @@ def data_request():
 def data_upload():
     if request.method == "POST":
         data = json.loads(request.data)
-        print(data)
-        return data,200
+        temperature = float(data["temperature"])
+        humidity = float(data["humidity"])
+        soil_humidity = float(data["soil_humidity"])
+        mydb = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          password="",
+          database="FIC"
+        )
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO Sensor_data (Temperature,Humidity,Soil_humidity) VALUES (%s, %s, %s)"
+        val = (f"{temperature}",f"{humidity}",f"{soil_humidity}")
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
+        # print("------------------------------------------")
+        # print(f"temperature = {temperature}")
+        # print(f"humidity = {humidity}")
+        # print(f"soil_humidity = {soil_humidity}")
+        # print("------------------------------------------")
+        return {"status" : "200 ok"},200
     else:
         return {"status" : "404 err"},404
+# if __name__ == '__main__':
+#     # run app in debug mode on port 5000
+#     app.run(debug=True, port=5000, host='0.0.0.0')
--- esp8266_code/FIC_esp8266/FIC_esp8266.ino
+++ esp8266_code/FIC_esp8266/FIC_esp8266.ino
@@ -1,9 +1,108 @@
-void setup() {
-  // put your setup code here, to run once:
+/**
+   PostHTTPClient.ino
 
+    Created on: 21.11.2016
+
+*/
+#include "DHTesp.h"
+#ifdef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
+#error Select ESP8266 board.
+#endif
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+DHTesp dht;
+/* this can be run with an emulated server on host:
+        cd esp8266-core-root-dir
+        cd tests/host
+        make ../../libraries/ESP8266WebServer/examples/PostServer/PostServer
+        bin/PostServer/PostServer
+   then put your PC's IP address in SERVER_IP below, port 9080 (instead of default 80):
+*/
+//#define SERVER_IP "10.0.1.7:9080" // PC address with emulation on host
+#define SERVER_IP "http://helloworld.3bbddns.com:42240/data_upload"
+//#define SERVER_IP "https://helloworld.3bbddns.com:42240/data_upload"
+#ifndef STASSID
+#define STASSID "KAI@2.4G"
+#define STAPSK "33479102"
+#endif
+const int analogInPin = A0;
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  String thisBoard= ARDUINO_BOARD;
+  Serial.println(thisBoard);
+  dht.setup(4, DHTesp::DHT11);
+  WiFi.begin(STASSID, STAPSK);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //------------------------------------------------
+  delay(dht.getMinimumSamplingPeriod());
+  // input humidity from DHT11
+  float humidity = dht.getHumidity();
+  // input temperature from DHT11
+  float temperature = dht.getTemperature();
+  // input soil humidity
+  int soil_sensor = analogRead(analogInPin);
+  //map value of analog from 1024-0 to 0-100
+  soil_sensor = map(soil_sensor, 0, 1024, 100, 0);
+  //------------------------------------------------
 
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+
+    WiFiClient client;
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+    http.begin(client, SERVER_IP);  // HTTP
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.print("[HTTP] POST...\n");
+    //create string 
+    String tmp = "";
+    String hum = "";
+    String soil = "";
+    //convert float/int to string for string concatenation 
+    tmp += temperature;
+    hum += humidity;
+    soil += soil_sensor;
+    // merge string to json from
+    String data = "{\"temperature\":\""+tmp+"\""+",\"humidity\":\""+hum+"\",\"soil_humidity\":\""+soil+"\"}";
+    //create request with POST methods
+    int httpCode = http.POST(data);
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+
+  delay(5000);
 }
