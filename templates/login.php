8983764ebdb87ee01d6a836f61ba91e12338ac10
FiFaKuBz <fifa.yanakorn@gmail.com>
2023-12-04 11:57:41 +0700

    login pages

--- templates/login.php
+++ templates/login.php
@@ -9,9 +9,21 @@
 </head>
 
 <body class="login">
-    <div class="login">
-    <h1 class="header">Login</h1>
-</div>
+    <div class="loginPage">
+        <div class="loginMid">
+            <h1 class="header">Login</h1>
+            <form action="role.php" method="post">
+                <label for="username">Gmail</label><br>
+                <input type="text" name="username" placeholder="Enter your gmail" required><br><br>
+                <label for="pass">Password</label><br>
+                <input type="password" name="pass" placeholder="Enter your password" required>
+                <br><br><br>
+                <div class="button">
+                <button type="submit" class="button">Login</button>
+            </div>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
